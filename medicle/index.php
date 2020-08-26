768a91afef0f349ce6b701ef33939c90718fed43
tanjum <69140369+tanjumf@users.noreply.github.com>
2020-08-26 10:11:04 +0600

    Add files via upload
    3rd commit

---
+++ medicle/index.php
@@ -0,0 +1,339 @@
+<html>
+<head>
+    <title>Project</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/slicknav.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" >
+
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="js/carofredsel.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+
+   <header>
+        <ul  class="menu" id="menu">
+            <li><a class="active" href="#slidr-carousel">Home</a></li>
+            <li><a href="#about">Our Story</a></li>
+            <li><a href="#intro">Services</a></li>
+            <li><a href="#testimonial">About Doctor's</a></li>
+            <li><a href="#contact">Take An Appoinment</a></li>
+        </ul>
+
+   </header>
+   <section class="slider">
+      <ul class="slidr-carousel" id="slidr-carousel">
+          <li class="img1">
+              <h2>Pet <span>Health</span> Care</h2>
+
+              <i class="fa fa-facebook"></i>
+              <i class="fa fa-instagram"></i>
+              <i class="fa fa-youtube"></i>
+              <p><br>
+            Here we served <span>better health care</span> service.<br>
+            Any<span> emergency need </span>you can find needed solution here...
+              </p><br>
+              <a href="one.php" class="btn btn-half">SIGN UP</a>
+          </li>
+           <li class="img2">
+              <h2>Pet<span>Health</span> Care</h2>
+
+              <i class="fa fa-facebook"></i>
+              <i class="fa fa-instagram"></i>
+              <i class="fa fa-youtube"></i>
+              <p><br>
+             Here we served <span>better health care</span> service.<br>
+            Any<span> emergency need </span>you can find needed solution here...
+              </p><br>
+              <a href="" class="btn btn-half">SIGN UP</a>
+          </li>
+
+      </ul>
+
+   </section>
+   <!-- end homepage -->
+
+   <section class="about-section" id="about">
+     <div class="img">
+      <div class="container">
+       <div class="row">
+            <div class="col-sm-12 text-center">
+               <h2>Our Story</h2>
+               <div class="sub-heading">
+                   <p>
+                       We can judge the heart of a man <br>by his treatment of animals.
+                   </p>
+               </div>
+
+            </div>
+
+         </div>
+         <div class="about1">
+
+
+
+              <p><b>In our site people can find pet which they want.We take order from the customer and We also provide them their pet's food,toy,
+                and other necessary things also.
+                We can help them to find a better one to give them details information about that
+                specific one which they would like to buy...
+                Our most important feature in this site will be to take care of pet
+                health. Visitor will find some health tips for their pets.</b></p>
+
+
+      </div>
+         </div>
+         </div>
+   </section>
+
+
+
+   <!--end story-->
+
+   <section class="intro-area white" id="intro">
+     <div class="img3" >
+      <div class="container">
+         <div class="row">
+            <div class="col-sm-12 text-center">
+               <h2>WORK WITH SMILE</h2>
+               <div class="sub-heading">
+                   <p>
+                       We can judge the heart of a man <br>by his treatment of animals.
+                   </p>
+               </div>
+
+            </div>
+
+         </div>
+           <div class="row">
+        <div class="col-md-3 text-center tx">
+          <div class="icon">
+            <i class="fa fa-stethoscope" aria-hidden="true"></i>
+
+          </div>
+          <h3>Emergency Care</h3>
+          <p>In this site You can find vets in emergency condition ...</p>
+
+        </div>
+        <div class="col-md-3 text-center tx">
+          <div class="icon">
+
+            <i class="fa fa-medkit" aria-hidden="true"></i>
+
+
+          </div>
+          <h3>Medicine</h3>
+          <p>you can find your needed medicine....</p>
+
+        </div>
+        <div class="col-md-3 text-center tx">
+          <div class="icon">
+            <i class="fa fa-user-md" aria-hidden="true"></i>
+
+          </div>
+          <h3>Advising for pet health</h3>
+          <p>You can find a suitable vets for your pets problem...</p>
+
+        </div>
+        <div class="col-md-3 text-center tx">
+          <div class="icon">
+            <i class="fa fa-plus-square" aria-hidden="true"></i>
+          </div>
+          <h3>Telemedicine</h3>
+          <p>by telemedicine health care by phone or computer we can offer you and your pet access to even more comprehensive medical care by connecting with doctors and specialists who can lend their expertise in the research, diagnosis, and treatment of specific cases....</p>
+
+        </div>
+
+
+      </div>
+
+      </div>
+      </div>
+
+   </section>
+   <!--end section-->
+
+   <section class="testimonials" id="testimonial">
+      <div class="container">
+         <div class="row">
+            <div class="col-sm-12 text-center">
+               <h2>About Doctor's</h2>
+               <div class="sub-heading">
+               <p>When I make my Patients SMILE<br>that's real success.</p>
+               </div>
+
+            </div>
+
+         </div>
+         <div class="row">
+            <div class="col-sm-12">
+               <div id="carousel-testimonials" class="carousel slide" data-ride="carousel">
+                  <ol class="carousel-indicators">
+                     <li data-target="#carousel-testimonials" data-slide-to="0" class="active"></li>
+                     <li data-target="#carousel-testimonials" data-slide-to="1" ></li>
+                     <li data-target="#carousel-testimonials" data-slide-to="2" ></li>
+
+
+                  </ol>
+
+                  <div class="carousel-inner">
+                     <div class="item active text-center">
+                         <img src="Images/depositphotos_33044395-stock-photo-doctor-smiling.jpg" class="center-block">
+                         <h1>Dr.Arifur Rabbi</h1>
+                         <h4>Veterinary Doctor</h4>
+                        <p>Pet animal Disease Diagnosis and Treatment.Pet animal Vaccination.Dog and Cat vaccine.<br>Dog and Cat Routine Deworming</p>
+
+                     </div>
+                     <div class="item text-center">
+                        <img src="Images/depositphotos_33044395-stock-photo-doctor-smiling.jpg" class="center-block">
+                         <h1>Dr.Arifur Rabbi</h1>
+                         <h4>Veterinary Doctor</h4>
+                        <p>Pet animal Disease Diagnosis and Treatment.Pet animal Vaccination.Dog and Cat vaccine.<br>Dog and Cat Routine Deworming</p>
+                     </div>
+                     <div class="item text-center">
+                        <img src="Images/depositphotos_33044395-stock-photo-doctor-smiling.jpg" class="center-block">
+                         <h1>Dr.Arifur Rabbi</h1>
+                         <h4>Veterinary Doctor</h4>
+                        <p>Pet animal Disease Diagnosis and Treatment.Pet animal Vaccination.Dog and Cat vaccine.<br>Dog and Cat Routine Deworming</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <!-- end About Doctors-->
+
+
+
+   <section class="contact-area" id="contact" >
+      <div class="img5">
+       <div class="container">
+          <div class="row">
+             <div class="col-sm-12 text-center">
+                <h2>Take An Appoinment</h2>
+                 <div class="sub-heading">
+               <p>The strength of the team is each individual member.<br>The strangth of each member is a team.</p>
+               </div>
+
+             </div>
+
+          </div>
+          <div class="row">
+             <div class="col-sm-3 devider">
+                <h3>Hospital contact</h3>
+                <div class="contact-address">
+                   <ul>
+                       <li>
+                           <i class="fa fa-home"></i>
+                           <div class="address-phone">
+                              <h4>address</h4>
+                              <span>1234 Mirpur road</span>
+                              <span>Dhaka,Bangladesh</span>
+
+                           </div>
+                       </li>
+                        <li>
+                           <i class="fa fa-phone"></i>
+                           <div class="address-phone">
+                              <h4>Phone</h4>
+                              <span>123456789</span>
+
+
+                           </div>
+                       </li>
+                        <li>
+                           <i class="fa fa-paper-plane"></i>
+                           <div class="address-phone">
+                              <h4>Email</h4>
+                              <span>abc@gmail.com</span>
+
+
+                           </div>
+                       </li>
+
+                   </ul>
+
+                </div>
+
+             </div>
+             <div class="col-sm-9">
+                <div class="contact-block">
+                   <h3>Patients INFO</h3>
+                   <form class="contact-form">
+                       <div class="row">
+                           <div class="col-sm-6">
+                               <div class="form-group">
+                                   <input type="text" class="form-control" placeholder="Pet Catagory" required="required">
+                               </div>
+                               <div class="form-group">
+                                   <input type="text" class="form-control" placeholder="Pet Age" required="required">
+                               </div>
+                               <div class="form-group">
+                                   <input type="text" class="form-control" placeholder="Required Doctor" required="required">
+                               </div>
+                           </div>
+                           <div class="col-sm-6">
+                               <div class="form-group">
+                                   <textarea class="font-control" placeholder="Problems Summary" required="required" ></textarea>
+                               </div>
+                               <div class="form-group">
+                                   <input type="submit" class="btn default-btn btn-block" value="Submit Quries">
+                               </div>
+                           </div>
+                       </div>
+                   </form>
+
+                </div>
+
+             </div>
+
+          </div>
+       </div>
+       </div>
+
+   </section>
+   <!-- end appoinment-->
+   <footer>
+      <div class="img6">
+       <div class="row">
+          <div class="col-md-4">
+
+          </div>
+          <div class="col-md-4 text-center">
+              <h1>PeTLovers</h1>
+              <h3>Thanks for visiting our page.</h3>
+              <p>Stay Connected with us</p>
+               <div class="team-social-network">
+                     <a href="service.html"><i class="fa fa-facebook"></i></a>
+                     <a href="service.html"><i class="fa fa-twitter"></i></a>
+                     <a href="service.html"><i class="fa fa-linkedin"></i></a>
+
+                  </div>
+          </div>
+          <div class="col-md-4">
+
+          </div>
+
+
+       </div>
+       </div>
+
+   </footer>
+
+
+
+    <script src="js/main.js"></script>
+    <script src="js/jquery.slicknav.min.js" type="text/javascript"></script>
+
+</body>
+</html>
